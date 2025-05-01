740dd3495f73a25acc639ab94e34fab7660550d0
ahmedanwar100 <ahmedanwer657@gmail.com>
2025-05-01 17:08:52 +0500

    Add project files.

---
+++ OldPhonePadDecoder.Core/OldPhonePadDecoder.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldPhonePadDecoder.Core
+{
+    public class OldPhonePadDecoder
+    {
+        public static string OldPhonePad(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentNullException(nameof(input), "Provided input should have a proper value.");
+            }
+
+            //to store the final result
+            var result = new StringBuilder();
+
+            //temporary storage for processing
+            var tempStorage = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char current = input[i];
+
+                if (current == '#')
+                    break;
+
+                if (current == '*')
+                {
+                    if (tempStorage.Length > 0)
+                    {
+                        //this will delete the digit(s) being typed
+                        tempStorage.Clear();
+                    }
+
+                    continue;
+                }
+
+                //if there is space then map the existing digits to characters
+                if (current == ' ')
+                {
+                    if (tempStorage.Length > 0)
+                    {
+                        result.Append(MapDigitToCharacter(tempStorage.ToString()));
+
+                        //clear the temporary storage
+                        tempStorage.Clear();
+                    }
+
+                    continue;
+                }
+
+                if (tempStorage.Length == 0 || tempStorage[tempStorage.Length - 1] == current)
+                {
+                    tempStorage.Append(current);
+                }
+                else
+                {
+                    //different digit found
+                    result.Append(MapDigitToCharacter(tempStorage.ToString()));
+
+                    //clear the temporary storage
+                    tempStorage.Clear();
+
+                    //add the new digit
+                    tempStorage.Append(current);
+                }
+            }
+
+            //if the temp storage still has some characters left then process them
+            if (tempStorage.Length > 0)
+            {
+                result.Append(MapDigitToCharacter(tempStorage.ToString()));
+
+                tempStorage.Clear();
+            }
+
+            return result.ToString();
+        }
+
+        private static char MapDigitToCharacter(string sequenceOfDigits)
+        {
+            char key = sequenceOfDigits[0];
+
+            int count = sequenceOfDigits.Length;
+
+            string keyCharacters = string.Empty;
+
+            switch (key)
+            {
+                case '2': { keyCharacters = "ABC"; break; }
+                case '3': { keyCharacters = "DEF"; break; }
+                case '4': { keyCharacters = "GHI"; break; }
+                case '5': { keyCharacters = "JKL"; break; }
+                case '6': { keyCharacters = "MNO"; break; }
+                case '7': { keyCharacters = "PQRS"; break; }
+                case '8': { keyCharacters = "TUV"; break; }
+                case '9': { keyCharacters = "WXYZ"; break; }
+                case '0': { keyCharacters = " "; break; }
+                default: keyCharacters = ""; break;
+            };
+
+            if (string.IsNullOrEmpty(keyCharacters))
+            {
+                throw new Exception($"Not found any character against the key:{key}");
+            }
+
+            int index = (count - 1) % keyCharacters.Length;
+
+            return keyCharacters[index];
+        }
+    }
+}
---
+++ OldPhonePadDecoder.Core/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using DecoderClass = OldPhonePadDecoder.Core.OldPhonePadDecoder;
+
+Console.WriteLine(DecoderClass.OldPhonePad("33#"));
+Console.WriteLine(DecoderClass.OldPhonePad("8 88777444666*664#"));
+Console.WriteLine(DecoderClass.OldPhonePad("4433555*5#"));
