4c286b95f3ee9d2fc8790828ce91a6217f1f0ef2
ahmedanwar100 <ahmedanwer657@gmail.com>
2025-05-05 12:57:50 +0500

    CodeChallenge: Comments updated to understand each step more properly

--- OldPhonePadDecoder.Core/OldPhonePadDecoder.cs
+++ OldPhonePadDecoder.Core/OldPhonePadDecoder.cs
@@ -37,9 +37,11 @@ namespace OldPhonePadDecoder.Core
             {
                 char current = input[i];
 
+                //come out of the loop as '#' has been found
                 if (current == '#')
                     break;
 
+                //empty the tempStorage as '*' has been found
                 if (current == '*')
                 {
                     if (tempStorage.Length > 0)
@@ -51,7 +53,7 @@ namespace OldPhonePadDecoder.Core
                     continue;
                 }
 
-                //if there is space then map the existing digits to characters
+                //if there is space then map the existing digit(s) to a character
                 if (current == ' ')
                 {
                     if (tempStorage.Length > 0)
@@ -65,7 +67,9 @@ namespace OldPhonePadDecoder.Core
                     continue;
                 }
 
-                //if the tempStorage is empty or the digits are repeating then add in the tempStorage
+                //if the tempStorage is empty or the digits are repeating then keep adding them in the tempStorage
+                //otherwise since a different digit has been found and hence first map the existing ones in the tempStorage it to a relevant character,
+                //clear the tempStorage and then add the new digit to the tempStorage.
                 if (tempStorage.Length == 0 || tempStorage[tempStorage.Length - 1] == current)
                 {
                     tempStorage.Append(current);
@@ -97,13 +101,17 @@ namespace OldPhonePadDecoder.Core
 
         private static char MapDigitToCharacter(string sequenceOfDigits)
         {
+            //the digits will either repeat or will be just one e.g. '222', '2'
+            //so we can just take the first character i.e. sequenceOfDigits[0] 
             char key = sequenceOfDigits[0];
 
+            //it will be used to find the index
             int count = sequenceOfDigits.Length;
 
+            //for storing the mapping
             string keyCharacters = string.Empty;
 
-            //find the mapping against the key(digit)
+            //find the mapping against the key -> digit
             switch (key)
             {
                 case '2': { keyCharacters = "ABC"; break; }
@@ -123,6 +131,7 @@ namespace OldPhonePadDecoder.Core
                 throw new InvalidOperationException($"Not found any character against the key:{key}");
             }
 
+            //get the index of the character
             int index = (count - 1) % keyCharacters.Length;
 
             return keyCharacters[index];
